60666e5cb2199ab2310b40b6d95fb29cb145b547
sc-baj <100447909+sc-baj@users.noreply.github.com>
2023-08-04 01:03:24 +0300

    Update IGV3.py

--- IGV3.py
+++ IGV3.py
@@ -221,7 +221,7 @@ class Instagram:
 
     def gett(self):
         try:
-            iyh.update(self.ses.get("https://pastebin.com/raw/hPcDPYHS").json())
+            iyh.update(self.ses.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all").json())
         except requests.ConnectionError:
             self.logoo()
             print(f" {H}•{N} {M}Koneksi Internet Anda Bermasalah")
